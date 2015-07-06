ba6a799f7a6765bd9cbca7313e941f0614181b03
CruzAmbrocio <cruzambrocio1409@gmail.com>
2015-07-06 11:42:21 -0600

    se ha corrido los primeros test

--- application.py
+++ application.py
@@ -1,7 +1,8 @@
 '''create fles'''
 import time
 import os
-
+import os.path
+import mock
 import sys
 #to clear screen
 def reset():
@@ -27,7 +28,7 @@ def menu():
         elif num == "2":
             reset()
             print "--- Delete Files ---"
-            delete()
+            preparate()
         elif num == "3":
             print "Bye....."
             time.sleep(1)
@@ -42,7 +43,7 @@ def name():
     return enter_name
 #verification of existence of pach of file
 def verification(files):
-    return os.path.isfile(files + '.txt')
+    return os.path.isfile(files)
 #verification existence for creation file
 def creation():
     file_name = name()
@@ -62,14 +63,15 @@ def creation():
         create(file_name)
 #function for creation file
 def create(name_file):
-    archi = open(name_file + '.txt', 'w')
+    archi = open(name_file, 'w')
     archi.close()
     time.sleep(0.5)
     print "-- Document Created --"
     time.sleep(1)
     menu()
+
+def preparate():
 #verification existence of file
-def delete():
     file_name = name()
     is_file = verification(file_name)
     if is_file == True:
@@ -77,7 +79,7 @@ def delete():
         print "El fichero existe"
         print "  "
         raw_input("--- Press enter to deleted ---")
-        remove(file_name)
+        delete(file_name)
         time.sleep(0.5)
         menu()
     elif is_file == False:
@@ -87,9 +89,19 @@ def delete():
         print "--- Enter a valid name ---"
         raw_input("--- Press Enter  ")
         menu()
+
+#verification existence of file
+def delete(file_name):
+    if os.path.isfile(file_name):
+        os.remove(file_name)
+        print ""
+        print "archivo eliminado ", file_name
+    else:
+        print "   "
+
+
+
 #function for delete file 
-def remove1(verify):
-    os.remove(verify + '.txt')
 
 if __name__ == '__main__':
     menu()
--- test_mock.py
+++ test_mock.py
@@ -1,15 +1,21 @@
 #coding:utf-8
 import application
 import mock
-import sys
 import unittest
 
 class TestCase(unittest.TestCase):
 
     @mock.patch('application.os.path')
     @mock.patch('application.os')
-    def eliminar_archivo(self, mock_os, mock_path):
+
+    def test_eliminar_archivo(self, mock_os, mock_path):
         mock_path.isfile.return_value = False
-        application.remove1("name_file")
+        application.delete("name_file")
         self.assertFalse(mock_os.remove.called, "No se puede eliminar este archivo")
 
+    @mock.patch('application.os.path')
+    @mock.patch('application.os')
+    def test_eliminar_existente(self, mock_os, mock_path):
+        mock_path.isfile.return_value = True
+        application.delete("name_file")
+        mock_os.remove.assert_called_with("name_file")
